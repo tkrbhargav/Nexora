d5795a836f105809595ae67e360e3c357e8fcbe5
tkrBhargav <tkr.connects@gmail.com>
2026-07-16 11:40:10 +0530

    chore: implemented query client provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router";
 import { AppProvider } from "./provider";
 import { router } from "./rotuer";
 
-export default function App(){
-  
-  return (
-    <AppProvider>
-      <RouterProvider router={router} />
-    </AppProvider>
-  )
+const queryClient = new QueryClient();
+
+export default function App() {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<AppProvider>
+				<RouterProvider router={router} />
+			</AppProvider>
+		</QueryClientProvider>
+	);
 }
